0248feac704c8b9473d61d74ed2878209e4d4d27
Rehn VVS Montage <info@rehnvvsmontage.se>
2026-09-10 07:20:00 +0200

    chore: add eslint lint script

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "react-hooks/purity": "warn",
+      "react-hooks/set-state-in-effect": "warn",
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "next-env.d.ts",
+    "node_modules/**",
+    "out/**",
+    "public/uploads/**",
+  ]),
+]);
+
+export default eslintConfig;
